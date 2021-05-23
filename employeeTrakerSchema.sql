1fdd6cd1caa9b2e634b5e782464fc180f0803cd4
Zahra Seyedi <za.seyedi@gmail.com>
2021-05-22 23:23:33 -0700

    creating tables

--- employeeTrakerSchema.sql
+++ employeeTrakerSchema.sql
@@ -1,3 +1,30 @@
 DROP DATABASE IF EXISTS employeeTrakerDB;
 CREATE DATABASE employeeTrakerDB;
 USE employeeTrakerDB;
+
+-- **Tables** --
+-- Employee --
+CREATE TABLE employee (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
+
+-- Role --
+CREATE TABLE roel (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INTEGER,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+-- Departmat --
+CREATE TABLE department (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
